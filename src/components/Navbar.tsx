18200161cadace98b0d6d2b964c871314d06abf1
Vivelune <shafiqueeee.atif@gmail.com>
2026-01-13 02:52:32 +0800

    3:Light and Darkmode Implemented

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,11 +1,18 @@
-import { LogOutIcon, MoonIcon, SettingsIcon, SquareMenu, UserIcon } from 'lucide-react'
+"use client"
+import { LogOutIcon, Moon, MoonIcon, SettingsIcon, SquareMenu, Sun, UserIcon } from 'lucide-react'
 import Link from 'next/link'
 import React from 'react'
 import { Avatar, AvatarFallback, AvatarImage } from './ui/avatar'
 import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuLabel, DropdownMenuSeparator, DropdownMenuTrigger } from './ui/dropdown-menu'
 import { Button } from './ui/button'
+import { useTheme } from 'next-themes'
 
 const Navbar = () => {
+
+
+    const {theme , setTheme} = useTheme()
+
+
   return(
     <nav className='p-4 flex items-center justify-between'>
       {/* collapse button */}
@@ -14,7 +21,29 @@ const Navbar = () => {
         <Link href='/'>
             Workstation
         </Link>
-        <MoonIcon/>
+
+        {/* Theme Menu */}
+        <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button variant="outline" size="icon">
+          <Sun className="h-[1.2rem] w-[1.2rem] scale-100 rotate-0 transition-all dark:scale-0 dark:-rotate-90" />
+          <Moon className="absolute h-[1.2rem] w-[1.2rem] scale-0 rotate-90 transition-all dark:scale-100 dark:rotate-0" />
+          <span className="sr-only">Toggle theme</span>
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent align="end">
+        <DropdownMenuItem onClick={() => setTheme("light")}>
+          Light
+        </DropdownMenuItem>
+        <DropdownMenuItem onClick={() => setTheme("dark")}>
+          Dark
+        </DropdownMenuItem>
+        <DropdownMenuItem onClick={() => setTheme("system")}>
+          System
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+        
         <DropdownMenu>
             <DropdownMenuTrigger>
             <Avatar>
